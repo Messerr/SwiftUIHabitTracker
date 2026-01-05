3da4454eac9d042185c5f486106411ee541c22e6
David Messer <MESSERR@users.noreply.github.com>
2026-01-04 20:18:08 -0500

    Initial commit

---
+++ SwiftUIHabitTracker/SwiftUIHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHabitTrackerApp.swift
+//  SwiftUIHabitTracker
+//
+//  Created by David Messer on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
